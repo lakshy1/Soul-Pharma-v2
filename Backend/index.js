adc237c3ff9e347f8cd00f9742d3cc93fdc96ead
lakshya1 <lakshyasehgal01@gmail.com>
2026-03-12 06:05:51 +0530

    Allow x-admin-route header in CORS

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "X-Admin-Route", "x-admin-route"]
 }));
 app.use(express.json({ limit: "1mb" }));
 
